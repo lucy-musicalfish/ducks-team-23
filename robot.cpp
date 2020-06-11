d02cf9169af6c6e64a72eec0a1706a299633ae21
Liam794 <66289137+Liam794@users.noreply.github.com>
2020-06-11 20:47:30 +1200

    issue of robot turning solved.
    I changed how the turning was calculated, giving the robot more turning range allowing it to go faster cause it can correct faster. Instead of doing  vleft/right + error/vleft/right i changed it to vleft/right + error/vleft/right * vleft/right/2 this is because at higher speeds the robot could not turn fast enough because of error/vright/left so i multiplied it by half the vleft/right
    
    Also changed where the robot was following the line, it was at half the robots vision, i made it closer(95) making the robot follow the line more closely.

--- robot.cpp
+++ robot.cpp
@@ -4,14 +4,15 @@
 void core(double vLeft, double vRight){
 	
     takePicture();
-	int row = 75;
+    
+	int row = 95; //changed row to 95, and used row. the 95 is closer to the robot and makes it better at fowlling the line. cause 50 is halfway into the robots view.
 	int average = (74 + 75 + 76 + 77) /4;
 	int error = 0;
 	double motorR = vRight;
 	double motorL = vLeft;
 	// for gets the current column and the gets the pixel at that column
     for (int column = 0; column < 150; column++){
-		int pix = get_pixel(cameraView, 50, column, 3);
+		int pix = get_pixel(cameraView, row, column, 3); //used row instead of "50"
 		int isWhite;
 		
 		// checks if pixel colour is white
@@ -29,16 +30,21 @@ void core(double vLeft, double vRight){
 	}
 	
 	// calculates the error correction for the robot for left motor wheel
+	
+	//changed to make it turn at high speeds. last version only changed the speed by a small amount. adding the * vleft or vRight made the turn have a bighger change. making it not come off track at high speeds cause the tunrning has a bigger range now.
 	if (error > 0){
-		motorL = vLeft + error/vLeft;
+		
+		motorL = vLeft + error/vLeft * vLeft/2;
+		
 	
 	// calculates the error correction for the robot for right motor wheel
 	} else if(error < 0){
-		motorR = vRight - error/vRight;
+		motorR = vRight - error/vRight * vRight/2;
 	}
 	
 	// sets motor speed
-	setMotors(motorL,motorR);   
+	setMotors(motorL,motorR); 
+	  
     std::cout<<"\nvLeft="<<motorL<<"  vRight="<<motorR<<std::endl;
     usleep(10000);
         
@@ -48,8 +54,8 @@ int main(){
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
 	}
-    double vLeft = 15.0;
-    double vRight = 15.0;
+    double vLeft = 30.0;
+    double vRight = 30.0;
     std::string robotVersion;
     
     while(1){ // Allows the user to choose what robot version to use 
